0119f4b72889f95af6fe74aa3a702699addf31cf
nllllibeth <sheina.02111@gmail.com>
2021-11-30 13:05:44 +0300

    https://codeforces.com/problemset/problem/71/A

--- 71A_long_words.py
+++ 71A_long_words.py
@@ -1,1 +1,11 @@
-ded 
+n = int(input())
+all_strings = []
+for i in range(n):
+    s1 = str(input())
+    if len(s1) > 10:
+        new_string = s1[0] + str((len(s1)-2)) + s1[-1]
+        all_strings.append(new_string)
+    else:
+        all_strings.append(s1)
+for i in all_strings:
+    print(i)
